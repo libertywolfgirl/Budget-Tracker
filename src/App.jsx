6806393cb2fcc3fcf96013007cf05b700add3441
Glitch (hello-cra) <none>
2020-10-23 15:53:23 +0000

    👦🏁 Checkpoint ./src/App.jsx:617503/5

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and changes will automatically happen!
+          Edit <code>src/App.js</code> and changes will automatically happen
         </p>
         <a
           className="App-link"
