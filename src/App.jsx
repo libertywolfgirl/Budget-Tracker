9dd5569a84b743ca4b1bddaef18d13aa608ca86f
Glitch (hello-cra) <none>
2021-03-31 19:24:20 +0000

    ⛲️📒 Checkpoint ./src/App.jsx:16231546/666 ./package.json:16231546/10

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import logo from "./logo.svg";
-//import "./App.css";
+import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-	return <p>Hello React!</p>;
-};
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and changes will automatically happen
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
